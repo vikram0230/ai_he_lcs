c740fe4e2cf156935f9a68137728c3c080932855
Abdul Zakkar <azakka2@uic.edu>
2023-12-07 16:09:45 -0600

    Retabbed, added age to the output data, changed output csv name to nlst_actual.

--- scripts/nlst_actual.py
+++ scripts/nlst_actual.py
@@ -26,7 +26,7 @@ will correspond to the headers above:
 - pid(screen.csv) corresponds with `pid`
 - study_yr(screen.csv) corresponds to `study_yr`
 - `days_to_diagnosis` will use candx_days(prsn.csv) MINUS scr_days0-2(prsn.csv)
-	- If candx_days is absent (no cancer), this value will be -1.
+    - If candx_days is absent (no cancer), this value will be -1.
 - gender(prsn.csv) corresponds with `gender`
 - race(prsn.csv) corresponds with `race`
 
@@ -56,7 +56,7 @@ def main():
         epilog="Example: nlst_actual.py \
         path/to/data_split.csv path/to/nlst_clinical_data_dir -o output_dir"
     )
-	parser.add_argument("datasplit", help="a CSV file provided by Sybil \
+    parser.add_argument("datasplit", help="a CSV file provided by Sybil \
         authors which identifies the split of each CT by patient ID. \
         Available on Google drive here: \
         https://drive.google.com/drive/folders\
@@ -74,114 +74,119 @@ def main():
     print("Clinical data directory:", args.clinical)
     print("Output directory:", args.outdir)
 
-	# Read in data split file provided by Sybil authors.
-	data_split = pd.read_csv(args.datasplit)
-	
-	# Find the CT screen and prsn file.
-	metadata_files = os.listdir(args.clinical)
-	screen_file = ""
-	prsn_file = ""
-	for file_name in metadata_files:
-		if "nlst" in file_name and "screen" in file_name:
-			screen_file = args.clinical + file_name
-		if "nlst" in file_name and "prsn" in file_name:
-			prsn_file = args.clinical + file_name
+    # Read in data split file provided by Sybil authors.
+    data_split = pd.read_csv(args.datasplit)
+    
+    # Find the CT screen and prsn file.
+    metadata_files = os.listdir(args.clinical)
+    screen_file = ""
+    prsn_file = ""
+    for file_name in metadata_files:
+        if "nlst" in file_name and "screen" in file_name:
+            screen_file = args.clinical + file_name
+        if "nlst" in file_name and "prsn" in file_name:
+            prsn_file = args.clinical + file_name
 
-	# Read in metadata CSVs
-	screen = pd.read_csv(screen_file) 
-	# We will iterate through these rows
-	
-	prsn = pd.read_csv(prsn_file)
+    # Read in metadata CSVs
+    screen = pd.read_csv(screen_file) 
+    # We will iterate through these rows
+    
+    prsn = pd.read_csv(prsn_file)
 
-	# This list will be a list of lists, each list being a row.
-	output = []	
+    # This list will be a list of lists, each list being a row.
+    output = [] 
 
-	# Iterate through each CT
-	for index, row in screen.iterrows():
-		# Initialize current row
-		current_row = []
+    # Iterate through each CT
+    for index, row in screen.iterrows():
+        # Initialize current row
+        current_row = []
 
-		# Add pid
-		current_row.append(row["pid"])
+        # Add pid
+        current_row.append(row["pid"])
 
-		# Add study year
-		study_yr = row["study_yr"]
-		current_row.append(study_yr)
-		scr_day_str = "scr_days" + str(study_yr)
-		screen_day = prsn.loc[prsn["pid"] == row["pid"]][scr_day_str]
-		# Make sure there is a day for this CT scan, otherwise do not record.
-		if screen_day.isnull().values[0]:
-			print(f">>> " + str(row["pid"]) + ":\n" +
-				"Missing " + scr_day_str + " value.\n" +
-				"Unable to include data for study year " +
-				str(study_yr) + ".")
-			continue 
+        # Add study year
+        study_yr = row["study_yr"]
+        current_row.append(study_yr)
+        scr_day_str = "scr_days" + str(study_yr)
+        screen_day = prsn.loc[prsn["pid"] == row["pid"]][scr_day_str]
+        # Make sure there is a day for this CT scan, otherwise do not record.
+        if screen_day.isnull().values[0]:
+            print(f">>> " + str(row["pid"]) + ":\n" +
+                "Missing " + scr_day_str + " value.\n" +
+                "Unable to include data for study year " +
+                str(study_yr) + ".")
+            continue 
 
-		# Add days to diagnosis (-1 for no cancer)
-		if (prsn.loc[prsn["pid"] == row["pid"]]["candx_days"
-			].isnull().values[0]
-		):
-			current_row.append(-1)
-		else:
-			screen_day = screen_day.astype(int).values[0]
-			days_to_diagnosis = prsn.loc[prsn["pid"] == row["pid"]][
-				"candx_days"].astype(int).values[0] - screen_day
-			current_row.append(days_to_diagnosis)
+        # Add days to diagnosis (-1 for no cancer)
+        if (prsn.loc[prsn["pid"] == row["pid"]]["candx_days"
+            ].isnull().values[0]
+        ):
+            current_row.append(-1)
+        else:
+            screen_day = screen_day.astype(int).values[0]
+            days_to_diagnosis = prsn.loc[prsn["pid"] == row["pid"]][
+                "candx_days"].astype(int).values[0] - screen_day
+            current_row.append(days_to_diagnosis)
 
-		# Add gender
-		gender = prsn.loc[prsn["pid"] == row["pid"]]["gender"].values[0]
-		current_row.append(gender)
-	
-		# Add race
-		race = prsn.loc[prsn["pid"] == row["pid"]]["race"].values[0]
-		current_row.append(race)
+        # Add age
+        age = prsn.loc[prsn["pid"] == row["pid"]]["age"].values[0]
+        current_row.append(age)
 
-		# Add yearly cancer columns
-		for year in range(1,7):
-			if (current_row[2] <= year * DAYS_IN_YEAR and
-				current_row[2] != -1
-			):
-				current_row.append(1)
-			else:
-				current_row.append(0)
+        # Add gender
+        gender = prsn.loc[prsn["pid"] == row["pid"]]["gender"].values[0]
+        current_row.append(gender)
+    
+        # Add race
+        race = prsn.loc[prsn["pid"] == row["pid"]]["race"].values[0]
+        current_row.append(race)
 
-		# Add the sybil data split.
-		# 0=train, 1=development, 2=test, 3=unseen
-		split = data_split.loc[data_split["pid"] == row["pid"]][
-			"split"]
-		if split.size == 0: # Not found in the split
-			current_row.append(99)
-		else:
-			split = split.values[0]
-			split_int = -1 
-			# if -1 found in output, 
-			# then there was an erroneous split table entry.
-			if split == "train": split_int = 0
-			elif split == "dev": split_int = 1
-			elif split == "test": split_int = 2
-			current_row.append(split_int)
+        # Add yearly cancer columns
+        for year in range(1,7):
+            if (current_row[2] <= year * DAYS_IN_YEAR and
+                current_row[2] != -1
+            ):
+                current_row.append(1)
+            else:
+                current_row.append(0)
 
-		# Add the whole complete row to the table.
-		output.append(current_row)		
+        # Add the sybil data split.
+        # 0=train, 1=development, 2=test, 3=unseen
+        split = data_split.loc[data_split["pid"] == row["pid"]][
+            "split"]
+        if split.size == 0: # Not found in the split
+            current_row.append(99)
+        else:
+            split = split.values[0]
+            split_int = -1 
+            # if -1 found in output, 
+            # then there was an erroneous split table entry.
+            if split == "train": split_int = 0
+            elif split == "dev": split_int = 1
+            elif split == "test": split_int = 2
+            current_row.append(split_int)
 
-	# Convert output into a Pandas DataFrame.
-	output_df = pd.DataFrame(output, columns=[
-		"pid",
-		"study_yr",
-		"days_to_diagnosis",
-		"gender",
-		"race",
-		"canc_yr1",
-		"canc_yr2",
-		"canc_yr3",
-		"canc_yr4",
-		"canc_yr5",
-		"canc_yr6",
-		"sybil_data_split"
-	]) 
-	# Save output CSV in output directory
-    output_df.to_csv(args.outdir + "/cleanup_nlst_for_sybil_out.csv",
-		index = False)
+        # Add the whole complete row to the table.
+        output.append(current_row)      
+
+    # Convert output into a Pandas DataFrame.
+    output_df = pd.DataFrame(output, columns=[
+        "pid",
+        "study_yr",
+        "days_to_diagnosis",
+        "age",
+        "gender",
+        "race",
+        "canc_yr1",
+        "canc_yr2",
+        "canc_yr3",
+        "canc_yr4",
+        "canc_yr5",
+        "canc_yr6",
+        "sybil_data_split"
+    ]) 
+    # Save output CSV in output directory
+    output_df.to_csv(args.outdir + "/nlst_actual.csv",
+        index = False)
 
 start = time.perf_counter()
 main()
